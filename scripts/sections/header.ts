aca4577f1495200392a0423f77404c2ad47dfeb3
Naseem Makiya <naseem.makiya@moovweb.com>
2013-02-13 14:02:02 -0800

    Beginning the header

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,44 @@
-# $('./body') {
-#   insert_top("header", class: "_header") {
-#     Move stuff here
-#   }
-# }
+$('./body') {
+  $(".//div[@id='Header']") {
+      # Move top menu into #Logo container
+      $("./div[@id='Logo']") {
+      move_here("../../../div[@id='TopMenu']", "bottom"){
+        $("./ul") {
+          # Remove unwanted top menu items from upper menu
+          $("./li[position() = 1 or position() > 2 and not(position()=last()-1)]") {
+            remove()
+          }     
+        }
+      }
+
+      # Inject header template
+      inject_after(read("../sections/header.html"))
+
+      # Fill search template
+      $("..//div[@class='mw_search']"){
+        # Move search form into container element
+        move_here("//div[@id='SearchForm']", "bottom"){
+          $("./p") {
+            remove()
+          }
+          $("./form") {
+            $("./label") {
+              remove()
+            }
+            # Change search button background image
+            $("./input[@type='image']") {
+              wrap("div", class: "mw_search_btn buttons-search")
+            }
+            $("./input[@type='text']") {
+              attribute("placeholder", "Search…")
+            } 
+          }     
+        }
+      }   
+    }                       
+    # Remove br
+    $("./br") {
+      remove()
+    }
+  }
+}
