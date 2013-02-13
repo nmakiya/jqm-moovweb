79051ce14262a9028a91cb25e9fe594071f8c6b6
Naseem Makiya <naseem.makiya@moovweb.com>
2013-02-13 14:04:01 -0800

    Creating the footer

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,5 +1,37 @@
-# $("./body") {
-#   insert_bottom("footer", class: "_footer") {
-#     Move stuff here
-#   }
-# }
+$("./body") {
+  $(".//div[@id='Footer']") {
+    $("./p") {
+      # Change current <p> tag to <div> tag
+      name("div")
+          
+      # Wrap text childeren with span tag
+      wrap_text_children("span")
+
+      # Insert placeholders for inserting elements
+      insert("div", class: "mw_currency")
+      insert("div", class: "mw_copy")
+      insert("div", class: "mw_moovweb")
+
+      # Move first two <span> elements into .mw_currency container
+      $("./div[@class='mw_currency']") {
+        move_here("../span[position() < 3]", "bottom")
+      }
+      
+      # Insert span with copyright text into .mw_copy container
+      $("./div[@class='mw_copy']") {
+        insert("span", "Copyright 2013 iGadgetCommerce.")
+      }
+      
+      # Insert <span> and <image> element into .mw_moovweb element
+      $("./div[@class='mw_moovweb']") {
+        insert("span", class: "mw_powered", "powered by:")
+        insert("div", class: "sprites-moovweb")
+      }     
+      
+      # Remove unwanded elements
+      $("./*[not(self::div)]") {
+        remove()
+      }     
+    } 
+  }
+}
