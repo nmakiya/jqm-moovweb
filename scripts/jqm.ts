6420069de171a4e27c9efd509c9ee875fce21186
Naseem Makiya <naseem.makiya@moovweb.com>
2013-04-16 15:30:11 -0700

    adding jqm.ts

---
+++ scripts/jqm.ts
@@ -0,0 +1,24 @@
+# remove all scripts lol
+# remove("//script[not(@data-mw_keep='true')]")
+
+jqm.init()
+
+$("/html/body") {
+  jqm.move_to_header(".//div[@id='Header']") {
+    $("./div[@id='Logo']") {
+      attribute("data-transition", "pop")
+    }
+  }
+  jqm.move_to_footer(".//div[@id='Footer']")
+  jqm.move_to_content(".//div[@id='Container']")
+}
+
+# remove default jqm padding on the content
+jqm.content() {
+  attribute("style", "padding: 0px")
+}
+
+# don't let jqm apply default css for all nodes
+$("/html/body//*[not(@data-role)]") {
+  attribute("data-role", "none")
+}
