b0545ff2c674e939dcca3f6bba194f8b68744c6e
Jeff Patzer <jeff.patzer@moovweb.com>
2013-04-17 10:40:47 -0700

    query mobile widget for the toggled on the home page

--- scripts/jqm.ts
+++ scripts/jqm.ts
@@ -3,40 +3,52 @@
 
 
 
-jqm.init()
+# jqm.init()
 
-$("/html/body") {
-  jqm.move_to_header(".//div[@id='Header']") {
-    $("./div[@id='Logo']") {
-      attribute("data-transition", "pop")
+$("/html") {
+  $("./head") {
+    remove("//script[contains(@src, 'jquery')]")
+    $("./script[1]") {
+      insert_before("script", src: "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js")
+      insert_before("script", src: asset("javascript/jqm-custom-config.js"))
+      insert_before("script", src: "http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js")
     }
+    insert_before("link", rel: "stylesheet", href: "http://code.jquery.com/mobile/1.3.1/jquery.mobile.structure-1.3.1.css")
   }
-  jqm.move_to_footer(".//div[@id='Footer']")
-  jqm.move_to_content(".//div[@id='Container']")
 }
 
-# remove default jqm padding on the content
-jqm.content() {
-  attribute("style", "padding: 0px")
-}
+# $("/html/body") {
+#   jqm.move_to_header(".//div[@id='Header']") {
+#     $("./div[@id='Logo']") {
+#       attribute("data-transition", "pop")
+#     }
+#   }
+#   jqm.move_to_footer(".//div[@id='Footer']")
+#   jqm.move_to_content(".//div[@id='Container']")
+# }
 
-# don't let jqm apply default css for all nodes
-$("/html/body//*[not(@data-role)]") {
-  attribute("data-role", "none")
-}
+# # remove default jqm padding on the content
+# jqm.content() {
+#   attribute("style", "padding: 0px")
+# }
 
-# reload uranium widgets, the default one isn't working...
-$("/html/body") {
-  insert("script", "x$(document).on('DOMContentLoaded', 
-    function(){ Ur.setup(); });")
-  insert("script", "$(document).ajaxComplete(function(){ Ur.setup(); });")
-}
+# # don't let jqm apply default css for all nodes
+# $("/html/body//*[not(@data-role)]") {
+#   attribute("data-role", "none")
+# }
 
-# attribute("data-ajax", "false")
+# # reload uranium widgets, the default one isn't working...
+# $("/html/body") {
+#   insert("script", "x$(document).on('DOMContentLoaded',
+#     function(){ Ur.setup(); });")
+#   insert("script", "$(document).ajaxComplete(function(){ Ur.setup(); });")
+# }
 
-# Move scripts to bottom of the body
-# because the HEAD doesn't get loaded everytime when using JQM
-# so if you want your scripts imported they need to be in the body
-$("/html//script") {
-  move_to("/html/body", "bottom")
-}
+# # attribute("data-ajax", "false")
+
+# # Move scripts to bottom of the body
+# # because the HEAD doesn't get loaded everytime when using JQM
+# # so if you want your scripts imported they need to be in the body
+# $("/html//script") {
+#   move_to("/html/body", "bottom")
+# }
--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,6 +1,6 @@
 $('./body') {
   $(".//div[@id='Header']") {
-    attribute("data-ur-set", "toggler")
+    # attribute("data-ur-set", "toggler")
       # Move top menu into #Logo container
       $("./div[@id='Logo']") {
       move_here("../../../div[@id='TopMenu']", "bottom"){
@@ -8,15 +8,15 @@ $('./body') {
           # Remove unwanted top menu items from upper menu
           $("./li[position() = 1 or position() > 2 and not(position()=last()-1)]") {
             remove()
-          }     
+          }
         }
       }
 
       # Inject header template
       inject_after(read("../sections/header.html"))
-      $("..//div[@class='mw_menu_btn']") {
-        attribute("data-ur-toggler-component", "button")
-      }
+      # $("..//div[@class='mw_menu_btn']") {
+      #   attribute("data-ur-toggler-component", "button")
+      # }
 
       # Fill search template
       $("..//div[@class='mw_search']"){
@@ -35,22 +35,22 @@ $('./body') {
             }
             $("./input[@type='text']") {
               attribute("placeholder", "Search…")
-            } 
-          }     
+            }
+          }
         }
-      }   
-    }                       
+      }
+    }
     # Remove br
     $("./br") {
       remove()
     }
 
-    # Move menu items into #Header
-    move_here("../div[@id='Menu']", "bottom"){
-      # Declare toggler content element which is enabled/diabled by toggler button
-      attribute("data-ur-toggler-component", "content")
-      # Move "Category List" element on bottom of #Menu element
-      move_here("../../div[@id='Wrapper']/div[@class='Left']/div[@id='SideCategoryList']", "bottom")
+    $("//div[@class='mw_menu_container']") {
+      # Move menu items into #Header
+      move_here("//div[@id='Menu']", "bottom"){
+        # Move "Category List" element on bottom of #Menu element
+        move_here("//div[@id='Wrapper']/div[@class='Left']/div[@id='SideCategoryList']", "bottom")
+      }
     }
   }
 }
