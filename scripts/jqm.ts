f380bbb10c51b8b689c3c9b92dd03a2dd3cce43a
Naseem Makiya <naseem.makiya@moovweb.com>
2013-04-18 12:17:03 -0700

    commenting out jqm code

--- scripts/jqm.ts
+++ scripts/jqm.ts
@@ -1,42 +1,38 @@
-# remove all scripts lol
-# remove("//script[not(@data-mw_keep='true')]")
 
+# jqm.init()
 
+# $("/html/body") {
+#   jqm.move_to_header(".//div[@id='Header']") {
+#     $("./div[@id='Logo']") {
+#       attribute("data-transition", "pop")
+#     }
+#   }
+#   jqm.move_to_footer(".//div[@id='Footer']")
+#   jqm.move_to_content(".//div[@id='Container']")
+# }
 
-jqm.init()
+# # remove default jqm padding on the content
+# jqm.content() {
+#   attribute("style", "padding: 0px")
+# }
 
-$("/html/body") {
-  jqm.move_to_header(".//div[@id='Header']") {
-    $("./div[@id='Logo']") {
-      attribute("data-transition", "pop")
-    }
-  }
-  jqm.move_to_footer(".//div[@id='Footer']")
-  jqm.move_to_content(".//div[@id='Container']")
-}
+# # don't let jqm apply default css for all nodes
+# $("/html/body//*[not(@data-role)]") {
+#   attribute("data-role", "none")
+# }
 
-# remove default jqm padding on the content
-jqm.content() {
-  attribute("style", "padding: 0px")
-}
+# # reload uranium widgets, the default one isn't working...
+# $("/html/body") {
+#   insert("script", "x$(document).on('DOMContentLoaded', 
+#     function(){ Ur.setup(); });")
+#   insert("script", "$(document).ajaxComplete(function(){ Ur.setup(); });")
+# }
 
-# don't let jqm apply default css for all nodes
-$("/html/body//*[not(@data-role)]") {
-  attribute("data-role", "none")
-}
+# # attribute("data-ajax", "false")
 
-# reload uranium widgets, the default one isn't working...
-$("/html/body") {
-  insert("script", "x$(document).on('DOMContentLoaded', 
-    function(){ Ur.setup(); });")
-  insert("script", "$(document).ajaxComplete(function(){ Ur.setup(); });")
-}
-
-# attribute("data-ajax", "false")
-
-# Move scripts to bottom of the body
-# because the HEAD doesn't get loaded everytime when using JQM
-# so if you want your scripts imported they need to be in the body
-$("/html//script") {
-  move_to("/html/body", "bottom")
-}
+# # Move scripts to bottom of the body
+# # because the HEAD doesn't get loaded everytime when using JQM
+# # so if you want your scripts imported they need to be in the body
+# $("/html//script") {
+#   move_to("/html/body", "bottom")
+# }
